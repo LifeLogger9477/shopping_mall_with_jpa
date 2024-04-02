3d237fb06c853c5aeca4a35109d7a592f0f15f60
LifeLogger9477 <private.mail.ms.lee@gmail.com>
2024-04-02 18:21:57 +0900

    QueryDsl 를 사용하여 페이징 처리 하기

--- src/main/java/com/book/shop/repository/ItemRepository.java
+++ src/main/java/com/book/shop/repository/ItemRepository.java
@@ -3,6 +3,7 @@ package com.book.shop.repository;
 import com.book.shop.entity.Item;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.querydsl.QuerydslPredicateExecutor;
 import org.springframework.data.repository.query.Param;
 
 import java.util.List;
@@ -11,7 +12,8 @@ import java.util.List;
  * author : ms.Lee
  * date   : 2024-04-01
  */
-public interface ItemRepository extends JpaRepository<Item, Long> {
+public interface ItemRepository extends JpaRepository<Item, Long>, 
+    QuerydslPredicateExecutor<Item> {
 
   List<Item> findByItemNm(String itemNm);
 
--- src/test/java/com/book/shop/repository/ItemRepositoryTest.java
+++ src/test/java/com/book/shop/repository/ItemRepositoryTest.java
@@ -3,6 +3,7 @@ package com.book.shop.repository;
 import com.book.shop.constant.ItemSellStatus;
 import com.book.shop.entity.Item;
 import com.book.shop.entity.QItem;
+import com.querydsl.core.BooleanBuilder;
 import com.querydsl.jpa.impl.JPAQuery;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import jakarta.persistence.EntityManager;
@@ -11,7 +12,11 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.test.context.TestPropertySource;
+import org.thymeleaf.util.StringUtils;
 
 import java.time.LocalDateTime;
 import java.util.List;
@@ -176,6 +181,71 @@ class ItemRepositoryTest {
     for (Item item : items) {
 
       System.out.println(item.toString());
+    }
+  }
+  
+  public void createItemList2() {
+
+    for (int i = 1; i <= 5; i++) {
+
+      Item item = new Item();
+      item.setItemNm("테스트 상품" + i);
+      item.setPrice(10000 + i);
+      item.setItemDetail("테스트 상품 상세 설명" + i);
+      item.setItemSellStatus(ItemSellStatus.SELL);
+      item.setStockNumber(100);
+      item.setRegTime(LocalDateTime.now());
+      item.setUpdateTime(LocalDateTime.now());
+
+      itemRepository.save(item);
+    }
+
+    for (int i = 6; i <= 10; i++) {
+
+      Item item = new Item();
+      item.setItemNm("테스트 상품" + i);
+      item.setPrice(10000 + i);
+      item.setItemDetail("테스트 상품 상세 설명" + i);
+      item.setItemSellStatus(ItemSellStatus.SOLD_OUT);
+      item.setStockNumber(0);
+      item.setRegTime(LocalDateTime.now());
+      item.setUpdateTime(LocalDateTime.now());
+
+      itemRepository.save(item);
+    }
+  }
+  
+  @Test
+  @DisplayName("상품 Querydsl 조회 테스트 #2")
+  public void queryDslTest2() {
+    
+    this.createItemList2();
+
+    BooleanBuilder booleanBuilder = new BooleanBuilder();
+
+    QItem item = QItem.item;
+    String itemDetail = "테스트 상품 상세 설명";
+    int price = 10003;
+    String itemSellState = "SELL";
+
+    booleanBuilder.and(item.itemDetail.like("%" + itemDetail + "%"));
+    booleanBuilder.and(item.price.gt(price));
+
+    if (StringUtils.equals(itemSellState, ItemSellStatus.SELL)) {
+
+      booleanBuilder.and(item.itemSellStatus.eq(ItemSellStatus.SELL));
+    }
+
+    Pageable pageable = PageRequest.of(0, 5);
+    Page<Item> itemPageResult = 
+        itemRepository.findAll(booleanBuilder, pageable);
+    System.out.println("total elements : " + itemPageResult.getTotalElements());
+
+    List<Item> resultItems = itemPageResult.getContent();
+
+    for (Item resultItem : resultItems) {
+
+      System.out.println(resultItem.toString());
     }
   }
 }
