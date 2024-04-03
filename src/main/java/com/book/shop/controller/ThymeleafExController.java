7a942fcdc5362ec4d87000341282a9ef67ac8c64
LifeLogger9477 <private.mail.ms.lee@gmail.com>
2024-04-03 17:35:10 +0900

    3.3 Thymeleaf 예제 진행하기 - 3.3.2 th:each 예제

--- src/main/java/com/book/shop/controller/ThymeleafExController.java
+++ src/main/java/com/book/shop/controller/ThymeleafExController.java
@@ -7,6 +7,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * author : ms.Lee
@@ -39,5 +41,26 @@ public class ThymeleafExController {
     model.addAttribute("itemDto", itemDto);
 
     return "thymeleafEx/thymeleafEx02";
+  }
+
+  @GetMapping(value = "/ex03")
+  public String thymeleafExample03(Model model) {
+
+    List<ItemDto> itemDtos = new ArrayList<>();
+
+    for (int i = 1; i <= 10; i++) {
+
+      ItemDto itemDto = new ItemDto();
+      itemDto.setItemDetail("상품 상세 설명" + i);
+      itemDto.setItemNm("테스트 상품" + i);
+      itemDto.setPrice(1000 * i);
+      itemDto.setRegTime(LocalDateTime.now());
+
+      itemDtos.add(itemDto);
+    }
+
+    model.addAttribute("itemDtos", itemDtos);
+
+    return "/thymeleafEx/thymeleafEx03";
   }
 }
